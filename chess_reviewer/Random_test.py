0e4b67b42020a003fdb4ab1d63917112159632b4
GabrielM <95947441+Gabrielkaos@users.noreply.github.com>
2023-08-03 18:21:56 +0800

    Delete chess_reviewer directory

--- chess_reviewer/Random_test.py
+++
@@ -1,6 +0,0 @@
-
-
-for i in range(10):
-    for j in range(i):
-        print("#",end="")
-    print()
--- chess_reviewer/Undo.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-class Undo:
-    def __init__(self):
-        self.move = 0
-        self.fifty_move = 0
-        self.en_passant = 0
-        self.castle_rights = 0
-        self.position_key = np.uint64(0)
